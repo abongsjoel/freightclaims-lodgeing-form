1cf5c5e5edb7e58c256650f0a33403c3e528db65
joel <joel@optimiz.claims>
2020-05-29 18:47:55 +0100

    Layout for card component established

--- src/Molecules/Card/index.js
+++ src/Molecules/Card/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Nav from './nav';
 
-const Card = () => (
-    <div className="w-1/2 h-48 shadow-2xl bg-white border border-gray-300">
-        <p>I am a card</p>
+
+const Card = (props) => (
+    <div className="w-1/2 shadow-2xl bg-white border border-gray-300">
+        <div className="m-5">
+            {props.children}
+        </div>
+        
+        <Nav />
     </div>
 );
 
---
+++ src/Molecules/Card/nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nav = () => (
+    <div>
+        <p>I am card navigation</p>
+    </div>
+);
+
+export default Nav;
