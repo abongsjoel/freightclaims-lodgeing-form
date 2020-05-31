a1222fd991bea1de49f33af0ac791b491d994e64
joel <joel@optimiz.claims>
2020-05-31 14:43:28 +0100

    Titlefont component implemented'

--- src/Organisms/Form/landing-screen.js
+++ src/Organisms/Form/landing-screen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Card from '../../Molecules/Card';
+import TitleFont from '../../Atoms/TitleFont';
 
 const LandingScreen = () => (
     <div className="w-full h-screen flex justify-center items-center">
-        <Card />
-    </div>
+        <Card>
+            <TitleFont required={true}>I am a ?</TitleFont>
+        </Card>
+    </div>  
 );
 
 export default LandingScreen;
