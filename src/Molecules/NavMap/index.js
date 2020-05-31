32c5cacc1f535a76d7b0aafde890813286df44a5
joel <joel@optimiz.claims>
2020-05-31 18:39:20 +0100

    NavMap computes active position automatically - Goodwork

--- src/Molecules/NavMap/index.js
+++ src/Molecules/NavMap/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 import Dot from '../../Atoms/Dot';
 
-const NavMap = () => (
+const NavMap = ({number}) => (
     <div className="w-4/5">
         <div className="w-full flex justify-center items-center relative">
             <hr className="w-full bg-neutral-500 border" />
             <div className="w-full flex justify-between absolute items-center">
-                <Dot active/>
-                <Dot />
-                <Dot />
-                <Dot />
-                <Dot />
+                <Dot active={number === "1"} />
+                <Dot active={number === "2"}/>
+                <Dot active={number === "3"}/>
+                <Dot active={number === "4"}/>
+                <Dot active={number === "5"}/>
             </div>
         </div>
     </div>
