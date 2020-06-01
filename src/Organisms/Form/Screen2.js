c0a47d70db5c68baac035dc731921f3a5cdd47c8
joel <joel@optimiz.claims>
2020-06-01 10:21:38 +0100

    Screen2 works

--- src/Organisms/Form/Screen2.js
+++ src/Organisms/Form/Screen2.js
@@ -3,11 +3,13 @@ import React from 'react';
 import InputText from '../../Atoms/InputElements/InputText';
 import FormScreens from './FormScreens';
 
-const Screen2 = () => (
+const Screen2 = ({previous, next}) => (
     <FormScreens 
         number = "2"
         title="What is your email ?"
         required={true}
+        previous={previous}
+        next={next}
     >
         <InputText instruction="Enter your email address here" />
     </FormScreens>
