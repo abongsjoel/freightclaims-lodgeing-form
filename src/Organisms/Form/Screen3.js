557e3b4bc0a1bc434e48804cd4c266d709142980
joel <joel@optimiz.claims>
2020-06-01 10:23:04 +0100

    Screen3 works

--- src/Organisms/Form/Screen3.js
+++ src/Organisms/Form/Screen3.js
@@ -3,11 +3,13 @@ import React from 'react';
 import InputText from '../../Atoms/InputElements/InputText';
 import FormScreens from './FormScreens';
 
-const Screen3 = () => (
+const Screen3 = ({previous, next}) => (
     <FormScreens 
         number = "3"
         title="What is your company name ?"
         required={true}
+        previous={previous}
+        next= {next}
     >
         <InputText instruction="Enter your company name here" />
     </FormScreens>
