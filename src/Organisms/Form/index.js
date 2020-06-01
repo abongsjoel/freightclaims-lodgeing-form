2fc4fc1cc77ef37dc67f55d68b3873464c937a7a
joel <joel@optimiz.claims>
2020-06-01 21:12:25 +0100

    First working version of animation

--- src/Organisms/Form/index.js
+++ src/Organisms/Form/index.js
@@ -63,7 +63,32 @@ const Form = () => {
             displayScreen = <LandingScreen />;
     }
 
-    return displayScreen;
+    // return displayScreen;
+
+      const pages = [
+            ({ style }) => <animated.div style={{ ...style }}><LandingScreen next={nextHandler} hidePrevious={true} /></animated.div>,
+            ({ style }) => <animated.div style={{ ...style }}><Screen1 previous={previousHandler} next={nextHandler} number={show} dotClicked={dotClickedHandler} /></animated.div>,
+            ({ style }) => <animated.div style={{ ...style }}><Screen2 previous={previousHandler} next={nextHandler} number={show} dotClicked={dotClickedHandler} /></animated.div>,
+            ({ style }) => <animated.div style={{ ...style }}><Screen3 previous={previousHandler} next={nextHandler} number={show} dotClicked={dotClickedHandler} /></animated.div>,
+            ({ style }) => <animated.div style={{ ...style }}><Screen4 previous={previousHandler} next={nextHandler} number={show} dotClicked={dotClickedHandler} /></animated.div>,
+            ({ style }) => <animated.div style={{ ...style }}><Screen5 previous={previousHandler} submit={submitHandler} number={show} dotClicked={dotClickedHandler} /></animated.div>,
+        ]
+  
+    // const [index, set] = useState(0)
+    // const onClick = useCallback(() => set(state => (state + 1) % 3), [])
+    const transitions = useTransition(show, p => p, {
+      from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+      enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+      leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+    })
+    return (
+      <div className="simple-trans-main">
+        {transitions.map(({ item, props, key }) => {
+          const Page = pages[item]
+          return <Page key={key} style={props} />
+        })}
+      </div>
+    )
 
 
 }
